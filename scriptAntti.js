8696577eeecd8677687967bc706b56295ddb2443
theAntifly <nuotant@gmail.com>
2022-01-19 10:37:20 +0200

    säätööö eyoo!
    säädin

---
+++ scriptAntti.js
@@ -0,0 +1,8 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("#body");
+
+  body.addEventListener("click", function () {
+    console.log("dingdong!");
+  });
+});
